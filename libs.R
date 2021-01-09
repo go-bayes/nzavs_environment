5d6cd32a7e4f4e75b5cdd7a355fe03325aa81a10
jacopastorius <joseph.bulbulia@gmail.com>
2021-01-09 15:58:01 +1300

    errors repaired

--- libs.R
+++ libs.R
@@ -1,54 +1,27 @@
 #libs
-
 # libraries
-library(dplyr)
-library(tidyverse)
-library(ggthemes)
-library(ggplot2)
-library(gghighlight)
-library(ggpubr)
-library(ggsci)
-library(sjstats)
-library(sjPlot)
-library(patchwork)
-library(table1)
-library(ggeffects)
-library(brms)
-library(rstan)
-library(bayesplot)
-library(lme4)
-library(car) # VIF
-library(performance)
-library(gghighlight)
-#library(texreg)
-library(parameters) # great tables, plots
-library(ggpubr)
+library("tidyverse")
+library("ggthemes")
+library("ggplot2")
+library("ggpubr")
+library("ggsci")
+library("sjstats")
+library("sjPlot")
+library("patchwork")
+library("ggeffects")
+library("brms")
+library("rstan")
+library("bayesplot")
+library("parameters") # great tables, plots
 library("mathjaxr")
-library(ggsci)
-library(sjstats)
-library(sjPlot)
-library(patchwork)
-library(table1)
 library("prettycode")
 library("styler")
-library(ggeffects)
-library(brms)
-library(rstan)
-library(bayesplot)
-library(lme4)
-library(car) # VIF
-library(see)# plots
-library(equatiomatic) 
-#library(texreg) # for latex
-library(parameters) # great tables, plots
-library(report)
-library(performance)
-library(ggpmisc)
-library(ggfortify)
-#library(magrittr) 
-library(lubridate)
-library(dagitty)
-library(ggdag)
+library("see")# plots
+library("bayestestR") # for latex
+library("performance")
+library("ggpmisc")
+library("dagitty")
+library("ggdag")
 
 # settings
 rstan_options(auto_write=TRUE)
