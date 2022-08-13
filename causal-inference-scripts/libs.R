3896def5d057ba01e1fcf7174cbf4bf1f5cc7c8a
Joseph Bulbulia <joseph.bulbulia@gmail.com>
2022-08-14 01:11:31 +1200

    Planning doc for papers
    Also added all ENVIRON variables.

---
+++ causal-inference-scripts/libs.R
@@ -0,0 +1,43 @@
+#libs.R
+
+#libraries ised
+library("dplyr")
+library("tidyr")
+library("here")
+library("skimr")
+#library("Amelia")
+library("ggplot2")
+library("purrr")
+library("patchwork")
+library("kableExtra")
+library("parameters")
+library("mice")
+library("table1")
+library("skimr")
+library("ggokabeito")   # color palette
+#library("brms") # bayesian estimation
+#library("rstan")
+library("ggpubr")
+#library("MatchThem")
+library("marginaleffects")
+library("mice")
+#library("cobalt")
+#library("WeightIt")
+#library("optmatch")
+#library("cmdstanr")
+library("formula.tools")
+library("splines") # estimation
+library("gghighlight") # highlight points
+library("parameters")
+library("naniar") # missing data
+library("conflicted")
+conflict_prefer("pool", "mice")
+conflict_prefer("filter", "dplyr")
+conflict_prefer("select", "dplyr")
+conflict_prefer("cbind", "base")
+#library("speedglm")
+library("stdReg") # g-computation
+#rstan_options(auto_write = TRUE) # bayesian estimation
+options(mc.cores = parallel::detectCores ()) # use all course
+theme_set(theme_pubclean()) # nice theme
+# theme_set(theme_classic())
---
+++ planning/planning.R
@@ -0,0 +1,122 @@
+# 
+# Environmental attitudes and Policy
+# Environmental efficacy
+# By taking personal action I believe I can make a positive difference to environmental problems.
+# I feel I can make a difference to the state of the environment.
+# Value - Protecting the environment
+# PROTECTING THE ENVIRONMENT (preserving nature)
+# Value - Unity with nature
+# UNITY WITH NATURE (fitting into nature)
+# Satisfaction with NZ environment
+# The quality of New Zealand’s natural environment.
+# Climate Change
+# Climate change is real.
+# Climate change is caused by humans.
+# I am deeply concerned about climate change.
+# Government regulation of carbon emissions
+# The New Zealand government should be involved in regulating carbon emissions.
+# Personal Environmental Sacrifice
+# Have you made sacrifices to your standard of living (e.g., accepted higher prices, driven less, conserved energy) in order to protect the environment?
+#   Have you made changes to your daily routine in order to protect the environment?
+#   Are you willing to make sacrifices to your standard of living (e.g., accept higher prices, drive less, conserve energy) in order to protect the environment?
+#   Are you willing to change your daily routine in order to protect the environment?
+#   Environmental sacrifice norm perception
+# Do you think most New Zealanders are willing to make sacrifices to their standard of living in order to protect the environment?
+#   Motorway Spending 
+# Increased government spending on new motorways.
+# Public Transport Subsidy 
+# Government subsidy of public transport.
+# Use of 1080 to control possums
+# Do you support the use of 1080 poison for possum control in New Zealand?
+#   Protection of NZ native species
+# Protecting New Zealand’s native species should be a national priority.
+# Quality of waterways
+# The quality and health of the waterways in your local region.
+
+
+# Planning:
+# 
+
+Controling in  time 8, 
+Environmental efficacy 
+Scripture 
+Sat NZ env
+ALL CLIMATE BELIEFS 
+1080
+PROTECTION NATIVE SPECIES 
+
+
+EXPOSURE 
+change in Scripture at 9 causes 
+RWA in TIME 11 --> 
+
+OUTCOMES 
+Environ efficacy 12
+Sat with Environ 12
+Climate change beliefs 12
+
+
+NEXT : direct effects 
+
+## 
+PROTECT  NATIVE SPECIES  11 
+1080 use 11 
+
+##
+PUBLIC SPENDING ROADS 11 (not controld)
+PUBLIC TRANSPORT 11 (not controlled)
+QUALITY WATERWAYS 11 (not controlled)
+
+## 
+REPEAT WITH CHURCH Attendance & Prayer
+
+##
+
+
+### 
+Controling for ENV norm in 5 does change in 6, 
+
+
+OUTCOMES 
+1. Environ efficacy in 8
+2. Sat with NZ environment 8
+3. Climate change real 8
+4. Climate change caused by humans 8
+5. Climate change concern 8
+7. 1080 8
+8. protect native species 8
+#9. Quality of NZ waterways 9 (no control)
+6. Environ sacrifices 9
+
+10. Motorway 11
+11. Public transport 11
+
+## 
+Repeat with 2 x climate change is real variables 
+
+
+
+
+
+
+
+
+
+
+
+
+
+ENV efficacy 19/12  --> 12
+
+SAT NZ environment -- ALL  12
+
+  
+
+## SAC NORM 
+5, 6 --> 7 / 12
+
+
+R
+
+
+#   
